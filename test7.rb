19f38c9c7e9f078069d26631d644db26f777a0fb
turtlecabbage-juno <householdappliance@metalfan.com>
2025-09-17 20:03:48 -0400

    oo it looks very programmy now

--- test7.rb
+++ test7.rb
@@ -1,34 +1,41 @@
-$correct = 0
-def math(question, answers)
-  attmp = 3
-  plural = " more attempts left"
+class Test
+  def initialize
+    @correct = 0
+  end
 
-  loop do
-    print(question)
-    guess = gets.chomp.downcase
+  def math(question, answers)
+    attmp = 3
+    plural = ' more attempts left'
 
-    if answers.include?(guess)
-      puts("Correct")
-      $correct += 1
-      break
-    elsif attmp == 1
-      puts("You stupid")
-      break
-    else
-      attmp -= 1
+    loop do
+      print(question)
+      guess = gets.chomp.downcase
 
-      if attmp == 1
-        plural = " more attempt left"
+      if answers.include?(guess)
+        puts('Correct')
+        @correct += 1
+        break
+      elsif attmp == 1
+        puts('You stupid')
+        break
       else
-      end
+        attmp -= 1
 
-      puts("Try again. " + attmp.to_s + plural)
-      next
+        plural = ' more attempt left' if attmp == 1
+
+        puts("Try again. #{attmp} #{plural}")
+        next
+      end
     end
+  end
+
+  def displaycorrect
+    puts("#{@correct} answers correct")
   end
 end
 
-math("What is 9*7? ", ["63"])
-math("What is 21/7? ", ["3"])
-math("Does your mom know you're gay? ", ["yes", "no"])
-puts($correct + " answers correct")
+test = Test.new
+test.math('What is 9*7? ', ['63'])
+test.math('What is 21/7? ', ['3'])
+test.math("Does your mom know you're gay? ", %w[yes no])
+test.displaycorrect
