bf4fa7ee46cf079c47bc2b45a168bf9da7cece39
tucab <turtlecabbage@protonmail.com>
2025-09-18 14:55:35 -0400

    behold the praise machine

---
+++ test8.rb
@@ -0,0 +1,42 @@
+class Praiser
+  def initialize
+    print("What is your name? ")
+    @name = gets.chomp
+    print("And what term do you prefer? girl, boy, etc. ")
+    @term = gets.chomp
+    @answers = ["me", "i am", @name.downcase]
+  end
+
+  def affirm
+    sleep(0.5)
+    puts("Yes you are")
+    sleep(1)
+    puts("Good #{@term}")
+    sleep(1)
+  end
+
+  def tryagain
+    puts("Uh-uh, try again silly #{@term}.")
+  end
+
+  def mommy
+    print("Who's a good #{@term}? ")
+    @answer = gets.chomp.downcase
+  end
+
+  def praise
+    loop do
+      mommy
+      if @answers.include?(@answer)
+        affirm
+        break
+      else
+        tryagain
+        next
+      end
+    end
+  end
+end
+
+praise = Praiser.new
+print(praise.praise)
