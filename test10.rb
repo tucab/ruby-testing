78eb3998792bdedd288e87f7261f377f5eb870f4
tucab <turtlecabbage@protonmail.com>
2025-09-20 12:01:31 -0400

    rudimentary file interactions

--- test10.rb
+++ test10.rb
@@ -4,30 +4,35 @@
 class ToDo
   def initialize
     @array = []
-    puts("Write \"done\" when done with entries.")
+    puts('Write "done" when done with entries.')
   end
 
   def stop_playing
-    return unless @array.length == 1 && @tasks == "done"
+    return unless @array.length == 1 && @tasks == 'done'
 
-    puts("bruh")
+    puts('bruh')
     exit
   end
 
   def quit
-    return unless @done == "quit"
+    return unless @done == 'quit'
 
-    puts("ok bye")
+    File.open('todo_example.txt', 'w') do |file|
+      file.puts(@array)
+    end
+
+    puts('ok bye')
+
     exit
   end
 
   def quitting_msg
-    puts("Type \"quit\" to exit")
-    puts("Type \"query\" to see remaining tasks")
+    puts('Type "quit" to exit and save remaining tasks to a file')
+    puts('Type "query" to see remaining tasks')
   end
 
   def input
-    print("What is on your todo list today? ")
+    print('What is on your todo list today? ')
     @array << gets.chomp.downcase
     @tasks = @array.last
     stop_playing
@@ -38,24 +43,24 @@ class ToDo
   end
 
   def query
-    return unless @done == "query"
+    return unless @done == 'query'
 
     puts(@array)
   end
 
   def collect
-    input until @tasks == "done"
+    input until @tasks == 'done'
     @array.pop
     output_count
   end
 
   def do_tasks
     until @array.count.zero?
-      print("When a task is completed, write it here. ")
+      print('When a task is completed, write it here. ')
       @done = gets.chomp.downcase
       quit
       query
-      puts("ok but that's not on the list") unless @array.include?(@done) || @done == "query"
+      puts("ok but that's not on the list") unless @array.include?(@done) || @done == 'query'
       @array.delete(@done)
       puts("#{@array.count} tasks left.") unless @array.count.zero?
     end
