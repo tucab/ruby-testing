13086c26d5c3b8666c8e0efd8be70ccc6f949530
tucab <turtlecabbage@protonmail.com>
2025-09-20 08:47:21 -0400

    fixed spacing issues

--- test6.rb
+++ test6.rb
@@ -1,27 +1,24 @@
 attmp = 3
-plural = " more attempts left"
+plural = ' more attempts left'
 q1 = 0
 
 loop do
-  print ("What is 9*7? ")
+  print('What is 9*7? ')
   guess = gets.chomp.to_i
 
   if guess == 63
-    puts ("Yippee")
+    puts('Yippee')
     q1 = 1
     break
-  elsif attmp == 1 then
-    puts ("You stupid")
+  elsif attmp == 1
+    puts('You stupid')
     break
   else
     attmp -= 1
 
-      if attmp == 1 then
-        plural = " more attempt left"
-      else
-        end
+    plural = ' more attempt left' if attmp == 1
 
-    puts ("Try again. " + attmp.to_s + plural)
+    puts('Try again. ' + attmp.to_s + plural)
     next
   end
 end
@@ -29,32 +26,27 @@ end
 if q1 == 1
   attmp = 3
   q2 = 0
-  
   sleep(1)
 
   loop do
-    
-    print ("What is 21*7? ")
+    print('What is 21*7? ')
     guess = gets.chomp.to_i
 
     if guess == 3
-      puts ("Yippee")
+      puts('Yippee')
       q2 = 1
       break
-    elsif attmp == 1 then
-      puts ("You stupid")
+    elsif attmp == 1
+      puts('You stupid')
       break
-    else # Why the hell do these look different but work the same? whatever.
+      # Why the hell do these look different but work the same? whatever.
+    else
       attmp -= 1
 
-      if attmp == 1 then
-        plural = " more attempt left"
-      end
+      plural = ' more attempt left' if attmp == 1
 
-      puts ("Try again. " + attmp.to_s + plural)
+      puts('Try again. ' + attmp.to_s + plural)
       next
-
     end
   end
-else
-  end
+end
